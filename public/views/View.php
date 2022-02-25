22b701138b487f10a2dd9cfe9b2623e2e951db23
Alexis1476 <halexisr20@hotmail.com>
2022-02-25 16:33:58 +0100

    Creation class View plus modification viewHome pour afficher le title

---
+++ public/views/View.php
@@ -0,0 +1,36 @@
+<?php
+
+class View
+{
+    private $_file;
+    private $_title;
+
+    public function __construct($action)
+    {
+        $this->_file = 'public/views/view' . $action . '.php';
+    }
+
+    public function displayView($data)
+    {
+        $content = $this->generateFile($this->_file, $data);
+
+        $view = $this->generateFile("public/views/template.php", ['title' => $this->_title, 'content' => $content]);
+
+        echo $view;
+    }
+
+    public function generateFile($file, $data)
+    {
+        if (file_exists($file)) {
+            extract($data);
+
+            ob_start();
+
+            require $file;
+
+            return ob_get_clean();
+        } else {
+            throw new Exception('Fichier ' . $file . ' introuvable');
+        }
+    }
+}
