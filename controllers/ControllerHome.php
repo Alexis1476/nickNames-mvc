62183d0b90940043d584b11afc92c0a2bc5153fb
Alexis1476 <halexisr20@hotmail.com>
2022-02-25 15:58:15 +0100

    Definition constructeur controllerHome + function teachers

--- controllers/ControllerHome.php
+++ controllers/ControllerHome.php
@@ -1,1 +1,20 @@
 <?php
+
+class ControllerHome
+{
+    private $_teacherModel;
+    private $_view;
+
+    public function __construct($url)
+    {
+        $this->teachers();
+    }
+
+    private function teachers()
+    {
+        $this->_teacherModel = new TeacherModel();
+        $teachers = $this->_teacherModel->getAllTeachers();
+
+        require_once 'public/views/viewHome.php';
+    }
+}
