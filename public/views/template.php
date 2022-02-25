12f9b89a5e633b67d8229beb8bceff8af4adf095
Alexis1476 <halexisr20@hotmail.com>
2022-02-25 17:43:14 +0100

    Modification lien style.css

--- public/views/template.php
+++ public/views/template.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="../resources/css/style.css" rel="stylesheet">
+    <link href="./public/resources/css/style.css" rel="stylesheet">
     <title><?= $title ?></title>
 </head>
 <body>
 <header>
+    <h1>Surnom des enseignants</h1>
+    <nav><h3>Zone pour le menu</h3></nav>
 </header>
 <?= $content ?>
 <footer>
