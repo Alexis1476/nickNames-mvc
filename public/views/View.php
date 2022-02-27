9f0faec558e2ce65b2f6bf26faa6b283d817e92b
Alexis1476 <halexisr20@hotmail.com>
2022-02-27 19:50:04 +0100

    getController + parametre controller in constructor

--- public/views/View.php
+++ public/views/View.php
@@ -2,12 +2,23 @@
 
 class View
 {
+    private $_controller;
+
+    /**
+     * @return mixed
+     */
+    public function getController()
+    {
+        return $this->_controller;
+    }
+
     private $_file;
     private $_title;
 
-    public function __construct($viewName)
+    public function __construct($viewName, $controller)
     {
         $this->_file = 'public/views/view' . $viewName . '.php';
+        $this->_controller = $controller;
     }
 
     public function displayView($data)
