070d3497c35c8a848ce5734c41c88acdec66d7c4
Alexis1476 <halexisr20@hotmail.com>
2022-03-02 15:30:37 +0100

    Modification titre du formulaire

--- public/views/viewEdit.php
+++ public/views/viewEdit.php
@@ -1,7 +1,7 @@
 <?php $this->_title = "Liste des enseignants"; ?>
 <div class="user-body">
     <form action="index.php?controller=teacher&action=submitEdit&idTeacher=<?= $teacher['idTeacher'] ?>" method="post">
-        <h3>Ajout d'un enseignant</h3>
+        <h3>Modification d'un enseignant</h3>
         <?php include_once 'templateFormTeacher.php' ?>
         <p>
             <input type="submit" value="Modifier">
